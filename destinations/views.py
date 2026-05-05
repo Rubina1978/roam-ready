bcc400add14d8b0334bba92f2e226686202e5b7f
Rubina1978 <hrubesova@gmail.com>
2026-05-05 19:26:13 +0100

    Add comment form and logic for users to contribute by comments

--- destinations/views.py
+++ destinations/views.py
@@ -1,7 +1,9 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Destination
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -20,12 +22,28 @@ def destination_detail(request, pk):
 
     destination = get_object_or_404(Destination, pk=pk)
     comments = destination.comments.all().order_by("-created_on")
-    comment_count = comments.filter(approved=True).count()
+    comment_count = destination.comments.filter(approved=True).count()
+
+#{% comment %}handling the comments{ % endcomment %}
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.destination = destination
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Thank you for your comment, your comment is awaiting approval'
+            )
+
+    comment_form = CommentForm()
 
     return render(
         request, 
         "destinations/destination_detail.html", 
         {"destination": destination,
          "comments":comments,
-         "comment_count": comment_count} 
+         "comment_count": comment_count,
+         "comment_form": comment_form} 
          )
