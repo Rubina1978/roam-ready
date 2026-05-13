3dbf438cb69fd73ca8b1ad69a5e51f4e95661a35
Rubina1978 <hrubesova@gmail.com>
2026-05-13 11:42:24 +0100

    styling environment

--- roamready/settings.py
+++ roamready/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 """
 color coding of messages 
 """
-MESSAGE_TAGS ={
+MESSAGE_TAGS = {
     messages.SUCCESS: 'alert-success',
     messages.ERROR: 'alert-danger',
 }
@@ -154,7 +154,7 @@ MESSAGE_TAGS ={
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
