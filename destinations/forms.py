8ad57ef19bc457814d84eaa06ecb90660cb29297
Rubina1978 <hrubesova@gmail.com>
2026-05-16 22:21:32 +0100

    add screenshot of linter admin.py

--- destinations/forms.py
+++ destinations/forms.py
@@ -2,11 +2,11 @@ from django import forms
 from .models import Comment, Tip
 
 
-
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('text',)
+
 
 class TipForm(forms.ModelForm):
     class Meta:
